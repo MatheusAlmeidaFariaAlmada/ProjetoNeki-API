cb9eca6e6de41be004a77c027ce02e61daaa1478
Matheus Almada <89466508+MatheusAlmeidaFariaAlmada@users.noreply.github.com>
2022-01-28 12:08:59 -0300

    Add files via upload

---
+++ src/main/java/org/neki/backend/projetoNeki/ProjetoNekiApplication.java
@@ -0,0 +1,13 @@
+package org.neki.backend.projetoNeki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNekiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/neki/backend/projetoNeki/ProjetoNekiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neki.backend.projetoNeki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
