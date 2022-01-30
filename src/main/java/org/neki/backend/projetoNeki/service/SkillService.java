575c5fe6d0800c894e1818b762f2ad2c118f9140
Matheus <matheus.almeida.faria.almada@gmail.com>
2022-01-29 22:10:53 -0300

    skill

---
+++ src/main/java/org/neki/backend/projetoNeki/service/SkillService.java
@@ -0,0 +1,29 @@
+package org.neki.backend.projetoNeki.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.neki.backend.projetoNeki.entity.SkillEntity;
+import org.neki.backend.projetoNeki.repository.SkillRepository;
+import org.neki.backend.projetoNeki.vo.SkillExibirVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SkillService {
+	
+	@Autowired
+	SkillRepository skillRepository;
+	
+	// Listar todos
+	public List<SkillExibirVO> buscarTodosService() {
+		List<SkillExibirVO> skillExibirVO = new ArrayList<SkillExibirVO>();
+		List<SkillEntity> skillEntity = skillRepository.findAll();
+		
+		for (SkillEntity skillEntity1 : skillEntity) {
+			SkillExibirVO skillVO = new SkillExibirVO(skillEntity1);
+			skillExibirVO.add(skillVO);
+		}
+		return skillExibirVO;
+	}
+}
